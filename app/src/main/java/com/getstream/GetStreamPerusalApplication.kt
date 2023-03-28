cf3f9c1996ea30cba63c5c79564000d65ab906aa
6879756e <6879756e@gmail.com>
2023-03-28 19:10:17 +0900

    Add `Hilt` library

--- app/src/main/java/com/getstream/GetStreamPerusalApplication.kt
+++ app/src/main/java/com/getstream/GetStreamPerusalApplication.kt
@@ -1,8 +1,10 @@
 package com.getstream
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class GetStreamPerusalApplication : Application() {
 
     override fun onCreate() {
