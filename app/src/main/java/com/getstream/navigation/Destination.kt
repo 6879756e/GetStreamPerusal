7bd4cadec0338ce922cc4deef62086b239189396
6879756e <6879756e@gmail.com>
2023-04-07 14:52:21 +0900

    Implement Destinations

--- app/src/main/java/com/getstream/navigation/Destination.kt
+++ app/src/main/java/com/getstream/navigation/Destination.kt
@@ -1,13 +1,37 @@
 package com.getstream.navigation
 
-interface Destination {
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Chat
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.MoreHoriz
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.capitalize
+import androidx.compose.ui.text.intl.Locale
+
+sealed interface Destination {
     val route: String
+    val imageVector: ImageVector
+    val label: String
+
+    companion object {
+        val allDestinations = listOf(Home, Chat, More)
+    }
 }
 
-object Login: Destination {
-    override val route = "login"
-}
-
-object Home: Destination {
+object Home : Destination {
     override val route = "home"
+    override val imageVector = Icons.Outlined.Home
+    override val label = route.capitalize(Locale.current)
+}
+
+object Chat : Destination {
+    override val route = "chat"
+    override val imageVector = Icons.Outlined.Chat
+    override val label = route.capitalize(Locale.current)
+}
+
+object More : Destination {
+    override val route = "more"
+    override val imageVector = Icons.Outlined.MoreHoriz
+    override val label = route.capitalize(Locale.current)
 }
