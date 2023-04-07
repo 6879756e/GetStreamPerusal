38f2e979e708822330f3b509a974129a82507960
6879756e <6879756e@gmail.com>
2023-04-07 14:52:37 +0900

    Implement BottomNavigation

--- app/src/main/java/com/getstream/GetStreamPerusalActivity.kt
+++ app/src/main/java/com/getstream/GetStreamPerusalActivity.kt
@@ -7,23 +7,32 @@ import android.view.ViewTreeObserver
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.getstream.login.LoginActivity
+import com.getstream.navigation.Chat
+import com.getstream.navigation.Destination
 import com.getstream.navigation.Home
+import com.getstream.navigation.More
 import com.getstream.ui.theme.GetStreamPerusalTheme
 import dagger.hilt.android.AndroidEntryPoint
 import io.getstream.chat.android.client.models.InitializationState
-import io.getstream.chat.android.compose.ui.channels.ChannelsScreen
-import io.getstream.chat.android.compose.ui.theme.ChatTheme
 
 @AndroidEntryPoint
 class GetStreamPerusalActivity : ComponentActivity() {
@@ -41,14 +50,16 @@ class GetStreamPerusalActivity : ComponentActivity() {
         observeIsLoginRequired()
 
         setContent {
-            val clientState by getStreamPerusalViewModel.clientState.collectAsStateWithLifecycle()
+            GetStreamPerusalTheme {
+                val clientState by getStreamPerusalViewModel.clientState.collectAsStateWithLifecycle()
 
-            if (clientState == InitializationState.COMPLETE) {
-                val navController = rememberNavController()
+                if (clientState == InitializationState.COMPLETE) {
+                    val navController = rememberNavController()
 
-                GetStreamNavHost(navController = navController)
+                    GetStreamPerusalActivityScreen(navController = navController)
 
-                content.viewTreeObserver.removeOnPreDrawListener(onFinishLoadingListener)
+                    content.viewTreeObserver.removeOnPreDrawListener(onFinishLoadingListener)
+                }
             }
         }
     }
@@ -62,34 +73,78 @@ class GetStreamPerusalActivity : ComponentActivity() {
         }
 
     @Composable
-    fun GetStreamNavHost(
+    fun GetStreamPerusalActivityScreen(
         navController: NavHostController,
         modifier: Modifier = Modifier,
     ) {
-        GetStreamPerusalTheme {
-            NavHost(
-                navController = navController,
-                startDestination = Home.route,
-                modifier = modifier,
-            ) {
-                composable(route = Home.route) {
-                    HomeScreen()
+        var currentDestination by remember { mutableStateOf<Destination>(Home) }
+
+        Column {
+            GetStreamPerusalNavHost(navController, modifier.weight(1f))
+
+            GetStreamPerusalBottomNavigation(
+                currentDestination = currentDestination,
+                onItemClicked = {
+                    currentDestination = it
+                    navController.navigate(it.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            inclusive = true
+                        }
+                        launchSingleTop = true
+                    }
+                }
+            )
+        }
+    }
+
+    @Composable
+    private fun GetStreamPerusalNavHost(
+        navController: NavHostController, modifier: Modifier
+    ) {
+        NavHost(
+            navController = navController,
+            startDestination = Home.route,
+            modifier = modifier,
+        ) {
+            composable(route = Home.route) {
+                Box(modifier = Modifier.fillMaxSize(1f)) {
+                    Text(text = Home.label, modifier = Modifier.align(Alignment.Center))
+                }
+            }
+            composable(route = Chat.route) {
+                Box(modifier = Modifier.fillMaxSize(1f)) {
+                    Text(text = Chat.label, modifier = Modifier.align(Alignment.Center))
+                }
+            }
+            composable(route = More.route) {
+                Box(modifier = Modifier.fillMaxSize(1f)) {
+                    Text(text = More.label, modifier = Modifier.align(Alignment.Center))
                 }
             }
         }
     }
 
     @Composable
-    fun HomeScreen() {
-        ChatTheme {
-            ChannelsScreen(
-                title = stringResource(id = R.string.app_name),
-                isShowingSearch = true,
-                onItemClick = { channel ->
-                    // TODO Start Messages Activity
-                },
-            )
+    private fun GetStreamPerusalBottomNavigation(
+        currentDestination: Destination,
+        onItemClicked: (Destination) -> Unit,
+    ) {
+        BottomNavigation {
+            Destination.allDestinations.forEach {
+                BottomNavigationItem(
+                    selected = it == currentDestination,
+                    onClick = { onItemClicked(it) },
+                    icon = {
+                        Icon(
+                            imageVector = it.imageVector,
+                            contentDescription = "Decorative icon for ${it.label}"
+                        )
+                    },
+                    label = {
+                        Text(text = it.label, style = MaterialTheme.typography.labelSmall)
+                    }
+                )
+            }
         }
     }
-
 }
