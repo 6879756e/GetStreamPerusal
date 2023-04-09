66139f789c36da934a2de5ef375524f42c7e0d00
6879756e <6879756e@gmail.com>
2023-04-09 12:01:26 +0900

    Refactor OnlineIndicator to be more elegant

--- app/src/main/java/com/getstream/ui/core/OnlineIndicator.kt
+++ app/src/main/java/com/getstream/ui/core/OnlineIndicator.kt
@@ -4,11 +4,12 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -35,36 +36,21 @@ fun OnlineIndicatorOnlinePreview() {
 }
 
 @Composable
-fun OnlineIndicator(isOnline: Boolean, size: Dp = 12.dp) {
-    if (isOnline) {
-        // TODO: Find an alternative (more elegant) method
-        Box(
-            modifier = Modifier
-                .size(size)
-                .clip(CircleShape)
-                .border(2.dp, MaterialTheme.colorScheme.background, CircleShape)
-        ) {
-            Box(
-                modifier = Modifier
-                    .size(size - 2.dp)
-                    .background(color = Color(0xFF003200), shape = CircleShape)
-                    .align(Alignment.Center)
-            )
+fun OnlineIndicator(isOnline: Boolean, modifier: Modifier = Modifier, size: Dp = 10.dp) {
+    Box(
+        modifier = modifier
+            .size(size)
+            .clip(CircleShape)
+            .background(MaterialTheme.colorScheme.background)
+            .padding(2.dp)
+            .clip(CircleShape)
+    ) {
+        val innerCircleModifier = if (isOnline) {
+            Modifier.background(Color(0xFF003200))
+        } else {
+            Modifier.border(1.dp, Color.LightGray, CircleShape)
         }
-    } else {
-        Box(
-            modifier = Modifier
-                .size(size = size)
-                .clip(CircleShape)
-                .background(MaterialTheme.colorScheme.background)
-        ) {
-            Box(
-                modifier = Modifier
-                    .size(size - 4.dp)
-                    .clip(CircleShape)
-                    .border(2.dp, Color.LightGray, shape = CircleShape)
-                    .align(Alignment.Center)
-            )
-        }
+
+        Box(modifier = innerCircleModifier.fillMaxSize())
     }
 }
